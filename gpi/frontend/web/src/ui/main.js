a3aca5624d51f2e35d5e6764435f6c9a4ca47599
Rit Li <get@rit.li>
2018-03-15 13:30:58 -0700

    Use @ui alias

--- gpi/frontend/web/src/ui/main.js
+++ gpi/frontend/web/src/ui/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import root from './root'
+import root from '@ui/components/root'
 import router from './router'
 
 Vue.config.productionTip = false
