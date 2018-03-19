40506236ae0e1b25f89b630cf97ef117a792d854
Rit Li <get@rit.li>
2018-03-19 16:11:21 -0700

    Configure eslint for mocha and chai

--- gpi/frontend/web/.eslintrc.js
+++ gpi/frontend/web/.eslintrc.js
@@ -7,6 +7,11 @@ module.exports = {
   },
   env: {
     browser: true,
+    mocha: true,
+
+  },
+  globals: {
+    expect: true,
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
