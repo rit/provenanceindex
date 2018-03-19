ba92f53b540feac78b55c6f5be65152064019c84
Rit Li <get@rit.li>
2018-03-19 15:35:43 -0700

    Use mocha and chai

---
+++ gpi/frontend/web/src/iso/compose/models.js
@@ -0,0 +1,10 @@
+class Artist {
+  constructor({ id, name }) {
+    this.id = id
+    this.name = name
+  }
+}
+
+module.exports = {
+  Artist,
+}
---
+++ gpi/frontend/web/src/iso/compose/models.spec.js
@@ -0,0 +1,10 @@
+const { Artist } = require('iso/compose/models')
+
+describe('Artist', () => {
+  it('has a name', () => {
+    let name = 'Dragon'
+    let artist = new Artist({ name })
+
+    expect(artist.name).to.equal(name);
+  });
+});
