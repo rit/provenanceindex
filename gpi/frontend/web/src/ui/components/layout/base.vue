e1f85d6839ad1850bf9d08bc41361955a43debbd
alyxrossetti <arossetti@getty.edu>
2018-03-22 15:56:37 -0700

    Merge with master

--- gpi/frontend/web/src/ui/components/layout/base.vue
+++ gpi/frontend/web/src/ui/components/layout/base.vue
@@ -3,6 +3,7 @@
     <header>
       <header-explore/>
       <header-logo/>
+      <header-nav/>
     </header>
 
     <section class="section">
@@ -18,6 +19,7 @@
 <script>
 import HeaderExplore from '@ui/header-explore.vue'
 import HeaderLogo from '@ui/header-logo.vue'
+import HeaderNav from '@ui/header-nav'
 import GettyFooter from '@ui/getty-footer.vue'
 
 export default {
@@ -25,6 +27,7 @@ export default {
   components: {
     HeaderExplore,
     HeaderLogo,
+    HeaderNav,
     GettyFooter,
   },
 }
