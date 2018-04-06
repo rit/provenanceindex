e1d26ad57059a1ac023682fabc5d309a5dc83d3c
alyxrossetti <arossetti@getty.edu>
2018-04-06 12:55:06 -0700

    Create person-title component

--- gpi/frontend/web/src/ui/components/core.js
+++ gpi/frontend/web/src/ui/components/core.js
@@ -3,10 +3,11 @@ import 'bulma/css/bulma.css'
 import Vue from 'vue'
 
 // Alphabetically import components
-import GettyFooter from '@ui/getty-footer.vue'
-import HeaderExplore from '@ui/header-explore.vue'
-import HeaderLogo from '@ui/header-logo.vue'
+import GettyFooter from '@ui/getty-footer'
+import HeaderExplore from '@ui/header-explore'
+import HeaderLogo from '@ui/header-logo'
 import HeaderNav from '@ui/header-nav'
+import PersonTitle from '@ui/person-title'
 import SearchBar from '@ui/search-bar'
 import Welcome from '@ui/welcome'
 
@@ -15,5 +16,6 @@ Vue.component('GettyFooter', GettyFooter)
 Vue.component('HeaderExplore', HeaderExplore)
 Vue.component('HeaderLogo', HeaderLogo)
 Vue.component('HeaderNav', HeaderNav)
+Vue.component('PersonTitle', PersonTitle)
 Vue.component('SearchBar', SearchBar)
 Vue.component('Welcome', Welcome)
