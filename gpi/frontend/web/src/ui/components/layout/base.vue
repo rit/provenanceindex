99296c1d16df0b8151a2667b001330d9d8d462e6
Rit Li <get@rit.li>
2018-04-12 11:31:38 -0700

    Fix merge regression

--- gpi/frontend/web/src/ui/components/layout/base.vue
+++ gpi/frontend/web/src/ui/components/layout/base.vue
@@ -13,21 +13,8 @@
 </template>
 
 <script>
-import ResourceType from 'iso/models/resource-type'
-import fixtures from 'iso/fixtures'
-import { map } from 'lodash'
-
 export default {
   name: 'LayoutBase',
-  data () {
-    return {
-      resources: [],
-    }
-  },
-  created () {
-    // TODO Replace fixtures.ResourceTypes with API response
-    this.resources = map(fixtures.ResourceTypes, resource => new ResourceType(resource))
-  },
 }
 </script>
 
