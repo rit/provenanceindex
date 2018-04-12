48ee5272edf019fa5895a384912b3938d82a7129
Rit Li <get@rit.li>
2018-04-12 11:36:43 -0700

    Use relative path for nested routes

--- gpi/frontend/web/src/ui/router/index.js
+++ gpi/frontend/web/src/ui/router/index.js
@@ -20,7 +20,7 @@ export default new Router({
       component: LayoutBase,
       children: [
         {
-          path: '/',
+          path: '',
           name: 'home',
           component: Welcome,
         },
