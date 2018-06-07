6ee9671a3886d3a8bd21ccabfd95cf913f9d384f
Rit Li <get@rit.li>
2018-06-07 14:31:58 -0700

    Put localizing code in its own makefile

--- gpi/frontend/web/localize.mk
+++ gpi/frontend/web/localize.mk
@@ -1,25 +1,27 @@
 LOCALES = en es
-POT = src/iso/generated/combined.pot
-SRCDIR = demo
-DESTDIR = src/iso/generated
-ALL_LOCALE_PO_FILES = $(shell find $(DESTDIR)/locale -name "*.po")
+SRCDIR = src
+GENDIR = src/iso/generated
+POT = $(GENDIR)/combined.pot
+TRANSLATIONS = src/iso/translations
+VUE_FILES = $(shell find src/ui -name "*.vue")
+ALL_LOCALE_PO_FILES = $(shell find $(TRANSLATIONS)/locale -name "*.po")
 
 
 localize: po_json
 
 po_template:
 	@# Extract messages from vue __SCRIPT__ tag
-	./localize.py extract_messages --input-dirs demo --output-file src/iso/generated/js.part.pot
+	./localize.py extract_messages --input-dirs $(SRCDIR) --output-file $(GENDIR)/js.part.pot
 	@# Extract messages from vue __TEMPLATE__ tag
-	npx gettext-extract --attribute v-translate --output src/iso/generated/html.part.pot demo/**.vue
+	npx gettext-extract --attribute v-translate --output $(GENDIR)/html.part.pot $(VUE_FILES)
 	@# Combine the PO files into one
-	msgcat -o src/iso/generated/combined.pot src/iso/generated/*.part.pot
+	msgcat -o $(GENDIR)/combined.pot $(GENDIR)/*.part.pot
 
 po_init: po_template
 	@# Create PO for each locale if not aleady exisits.
 	@# msginit treats --lang=en a bit differently: it will set the msgstr value to msgid.
 	@for lang in $(LOCALES); do \
-		export PO_FILE=$(DESTDIR)/locale/$$lang/LC_MESSAGES/app.po; \
+		export PO_FILE=$(TRANSLATIONS)/locale/$$lang/LC_MESSAGES/app.po; \
 		mkdir -p $$(dirname $$PO_FILE); \
 		[ -f $$PO_FILE ] || msginit --no-translator --locale=$$lang --input=$(POT) --output-file=$$PO_FILE; \
 	done;
@@ -27,11 +29,11 @@ po_init: po_template
 po_merge: po_template
 	@# When new phrases are added to the template, we need to merge them into each locale
 	@for lang in $(LOCALES); do \
-		export PO_FILE=$(DESTDIR)/locale/$$lang/LC_MESSAGES/app.po; \
+		export PO_FILE=$(TRANSLATIONS)/locale/$$lang/LC_MESSAGES/app.po; \
 		msgmerge --lang=$$lang --update $$PO_FILE $(POT); \
 	done;
 
 po_json: po_merge
-	@@npx gettext-compile --output $(DESTDIR)/locale/translations.json $(ALL_LOCALE_PO_FILES)
+	@@npx gettext-compile --output $(TRANSLATIONS)/translations.json $(ALL_LOCALE_PO_FILES)
 
 .PHONY: po_template po_merge po_json localize
