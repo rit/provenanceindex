86073bbcc1122088becb4c97a5d7a2bd6a522c5e
Rit Li <get@rit.li>
2018-06-21 14:04:38 -0700

    Show related count badge for purchase event

--- gpi/frontend/web/src/iso/models/purchase.js
+++ gpi/frontend/web/src/iso/models/purchase.js
@@ -1,16 +1,47 @@
 const Resource = require('./resource')
 
 class Purchase extends Resource {
+  constructor (props) {
+    super(props)
+    this.qualifier = 'purchase'
+
+    // Required props
+    this.id = props.id
+    this.label = props.label
+
+    // Optional props
+    this.dateOfTransaction = props.dateOfTransaction
+    this.buyers = props.buyers
+    this.sellers = props.sellers
+    this.artObjects = props.artObjects
+    this.payments = props.payments
+
+    // Vue specific helper props
+    this.ui = {
+      // TODO this should be 'exchange'
+      icon: 'handshake',
+    }
+  }
+
   blurbFields () {
     return [
       ['Purchased from', 'Some seller'],
-      ['Date', 'February 22, 1888'],
+      ['Date', this.dateOfTransaction],
       ['Objects', 'object 1, object 2'],
     ]
   }
 
   heading () {
-    return 'Object: Dragon Painting'
+    return `${this.label}`
+  }
+
+  relatedCountBadges () {
+    // TODO: Determine how to get these data
+    return [
+      { type: 'document', count: 20 },
+      { type: 'event', count: 30 },
+      { type: 'object', count: 40 },
+    ]
   }
 }
 
--- gpi/frontend/web/src/iso/models/purchase.spec.js
+++ gpi/frontend/web/src/iso/models/purchase.spec.js
@@ -1,16 +1,26 @@
+const purchasejson = require('iso/json-ld/purchase')
 const Purchase = require('./purchase')
-const { map } = require('lodash')
+const { parser } = require('iso/marq')
 
 describe('Purchase', function () {
+  let props
+  beforeEach(async function () {
+    let res = await parser.walk({ doc: purchasejson })
+    expect(res.data).to.be.ok
+    props = res.data.purchase
+  })
+
   it('has blurb fields', function () {
-    let purchase = new Purchase()
+    let purchase = new Purchase(props)
     let fields = purchase.blurbFields()
-    let labels = map(fields, ([label, _]) => label)
-    expect(labels).to.eql(['Purchased from', 'Date', 'Objects'])
+
+    expect(fields[0]).to.eql(['Purchased from', 'Some seller'])
+    expect(fields[1]).to.eql(['Date', '1939-04-10'])
+    expect(fields[2]).to.eql(['Objects', 'object 1, object 2'])
   })
 
   it('has heading', function () {
-    let purchase = new Purchase()
-    expect(purchase.heading()).to.eql('Object: Dragon Painting')
+    let purchase = new Purchase(props)
+    expect(purchase.heading()).to.eql('purchasing')
   })
 })
